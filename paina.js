b7380a8fb3e8be908e9fb3e547015018014850b4
Nelson I. Gonzalez <nigm2005@gmail.com>
2016-01-27 23:38:17 -0600

    First commit

---
+++ paina.js
@@ -0,0 +1,15 @@
+var tokens = [
+	{token: "NUMBER", value: "10"},
+	{token: "PLUS",   value: "+"},
+	{token: "NUMBER", value: "10"},
+	{token: "PLUS",   value: "+"},
+	{token: "NUMBER", value: "15"}
+];
+
+function ast() {
+	var ast = {};
+}
+
+function run() {
+
+}
